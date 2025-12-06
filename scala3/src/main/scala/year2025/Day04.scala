b6d2e61c626516e0b6c94c47acb057bb3cc3ef00
Dmitrijs Ivancenko <dmitry1991@gmail.com>
2025-12-06 19:15:28 +0200

    Day 4 - Change to Set

--- scala3/src/main/scala/year2025/Day04.scala
+++ scala3/src/main/scala/year2025/Day04.scala
@@ -10,21 +10,21 @@ object Day04 extends App:
 
   def part2(input: String) = loop(buildGrid(input), 0)
 
-  def buildGrid(s: String): Map[Pos, Boolean] =
-    s.linesIterator.zipWithIndex.foldLeft(Map.empty[Pos, Boolean]) { case (grid, (row, x)) =>
+  def buildGrid(s: String): Set[Pos] =
+    s.linesIterator.zipWithIndex.foldLeft(Set.empty[Pos]) { case (grid, (row, x)) =>
       grid ++ row.zipWithIndex.foldLeft(grid) { case (grid, (char, y)) =>
-        if (char == '@') grid.updated(Pos(x, y), true) else grid
+        if (char == '@') grid + Pos(x, y) else grid
       }
     }
 
-  def accessibleRolls(grid: Map[Pos, Boolean]): Int =
-    grid.count { case (pos, _) => pos.neighbors.count(p => grid.contains(p)) < 4 }
+  def accessibleRolls(grid: Set[Pos]): Int =
+    grid.count { pos => pos.neighbors.count(p => grid.contains(p)) < 4 }
 
-  def removableRolls(grid: Map[Pos, Boolean]): Iterable[Pos] =
-    grid.collect { case (pos, _) if pos.neighbors.count(p => grid.contains(p)) < 4 => pos }
+  def removableRolls(grid: Set[Pos]): Iterable[Pos] =
+    grid.collect { case pos if pos.neighbors.count(p => grid.contains(p)) < 4 => pos }
 
   @tailrec
-  def loop(grid: Map[Pos, Boolean], removed: Int): Int =
+  def loop(grid: Set[Pos], removed: Int): Int =
     val toRemove = removableRolls(grid)
     if (toRemove.nonEmpty) loop(grid.removedAll(toRemove), removed + toRemove.size)
     else removed
